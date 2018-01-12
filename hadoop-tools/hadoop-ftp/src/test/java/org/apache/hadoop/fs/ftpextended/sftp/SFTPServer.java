b9faa56a601b22b2e0b81ecd26daf3bd0b9d8211
Waldmann <lukas.waldmann@merck.com>
2018-01-12 19:47:52 +0100

    Base for hadoop 2.9

--- hadoop-tools/hadoop-ftp/src/test/java/org/apache/hadoop/fs/ftpextended/sftp/SFTPServer.java
+++ hadoop-tools/hadoop-ftp/src/test/java/org/apache/hadoop/fs/ftpextended/sftp/SFTPServer.java
@@ -22,19 +22,15 @@ import java.util.Arrays;
 import java.util.List;
 import org.apache.ftpserver.FtpServerFactory;
 import org.apache.hadoop.fs.ftpextended.common.Server;
-import org.apache.sshd.server.SshServer;
+import org.apache.sshd.SshServer;
 import org.apache.sshd.common.NamedFactory;
 import org.apache.sshd.common.file.virtualfs.VirtualFileSystemFactory;
-import org.apache.sshd.common.io.IoSession;
-import org.apache.sshd.common.io.IoWriteFuture;
 import org.apache.sshd.server.Command;
-import org.apache.sshd.server.auth.UserAuth;
-import org.apache.sshd.server.auth.UserAuthNoneFactory;
+import org.apache.sshd.server.UserAuth;
+import org.apache.sshd.server.auth.UserAuthPassword;
 import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
 import org.apache.sshd.server.session.ServerSession;
-import org.apache.sshd.server.session.ServerSessionImpl;
-import org.apache.sshd.server.session.SessionFactory;
-import org.apache.sshd.server.subsystem.sftp.SftpSubsystemFactory;
+import org.apache.sshd.server.sftp.SftpSubsystem;
 
 /**
  * SFTP test server.
@@ -47,33 +43,15 @@ public class SFTPServer implements Server {
     sshd = SshServer.setUpDefaultServer();
     // ask OS to assign a port
     sshd.setPort(0);
-    sshd.setSessionFactory(new SessionFactory(sshd) {
-      @Override
-      protected ServerSessionImpl doCreateSession(IoSession ioSession) throws
-              Exception {
-        return new ServerSessionImpl(this.getServer(), ioSession) {
-          @Override
-          protected IoWriteFuture sendIdentification(String ident) {
-            try {
-             // wait a bit so connection is established before sending ident
-             // Seems to be a bug in littleproxy
-              Thread.sleep(50); // NOSONAR
-            } catch (InterruptedException ex) {
-            }
-            return super.sendIdentification(ident);
-          }
-        };
-      }
-    });
     sshd.setKeyPairProvider(new SimpleGeneratorHostKeyProvider());
 
     File rootDir = new File(root);
     rootDir.mkdir();
     sshd.setFileSystemFactory(new VirtualFileSystemFactory(
-            rootDir.toPath()));
+            rootDir.getAbsolutePath()));
     List<NamedFactory<UserAuth>> userAuthFactories
             = new ArrayList<>();
-    userAuthFactories.add(new UserAuthNoneFactory());
+    userAuthFactories.add(new UserAuthPassword.Factory());
 
     sshd.setUserAuthFactories(userAuthFactories);
 
@@ -83,7 +61,7 @@ public class SFTPServer implements Server {
             username.equals("user") && password.equals("password")
     );
     sshd.setSubsystemFactories(
-            Arrays.<NamedFactory<Command>>asList(new SftpSubsystemFactory()));
+            Arrays.<NamedFactory<Command>>asList(new SftpSubsystem.Factory()));
 
     sshd.start();
     port = sshd.getPort();
@@ -94,7 +72,7 @@ public class SFTPServer implements Server {
     if (sshd != null) {
       try {
         sshd.stop();
-      } catch (IOException e) {
+      } catch (InterruptedException e) {
         // ignore
       }
     }
